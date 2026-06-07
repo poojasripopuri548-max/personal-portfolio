a35e783ee14689ed96f5a42c2ed4427820d201fa
poojasripopuri548-max <poojasripopuri548@gmail.com>
2026-06-07 14:43:17 +0530

    Frontend ready for Netlify

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-fetch("http://localhost:5000/projects")
+fetch("https://personal-portfolio-b2gb.onrender.com/api/projects")
   .then(response => response.json())
   .then(data => {
-
     const projectList = document.getElementById("project-list");
 
     data.forEach(project => {
-
       projectList.innerHTML += `
         <div class="project">
           <h3>${project.title}</h3>
           <p>${project.description}</p>
         </div>
       `;
-
     });
-
   })
-  .catch(error => {
-    console.log("Error:", error);
-  });
+  .catch(error => console.log(error));
